82cb2f973d1597a294035573d6b492d7f95b6029
zineb zarda <zinebzarda1@gmail.com>
2024-08-27 09:45:08 +0100

    first commit

---
+++ ConstructionXpertPart2/src/main/java/com/ConstructionXpertPart2/ConstructionXpertPart2Application.java
@@ -0,0 +1,13 @@
+package com.ConstructionXpertPart2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConstructionXpertPart2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConstructionXpertPart2Application.class, args);
+	}
+
+}
